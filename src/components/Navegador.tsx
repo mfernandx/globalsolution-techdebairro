6572baad97122b3ee89db492c7d769c66305dbfb
mfernandx <mfernanda2904@gmail.com>
2025-11-18 00:14:46 -0300

    Segundo Commit

--- src/components/Navegador.tsx
+++ src/components/Navegador.tsx
@@ -1,6 +1,33 @@
+import logo from '/logotipo-techdebairro.png'
+
 export default function Navegador() {
     return(
         <>
+        <nav className="flex justify-center items-center text-xl p-5  ">
+
+            <img className="w-40 mr-10" src={logo} />
+            
+            <ul className="flex justify-center flex-1 list-none mr-50">
+            <li><a className="no-underline text-[#444444] font-bold inline-block px-5 py-2.5 transition-all duration-200
+                border-b-transparent hover:border-b-10 hover:border-[#2000fc]" href="/"> Início </a></li>
+
+            <li><a className="no-underline text-[#444444] font-bold inline-block px-5 py-2.5 transition-all duration-200
+                border-b-transparent hover:border-b-10 hover:border-[#2000fc]" href="/funcionalidades"> Funcionalidades </a></li>
+
+            <li><a className="no-underline text-[#444444] font-bold inline-block px-5 py-2.5 transition-all duration-200
+                border-b-transparent hover:border-b-10 hover:border-[#2000fc]"href="/prototipo"> Protótipo </a></li>
+
+            <li><a className="no-underline text-[#444444] font-bold inline-block px-5 py-2.5 transition-all duration-200
+                border-b-transparent hover:border-b-10 hover:border-[#2000fc]" href="/faq"> FAQ </a></li>
+
+            <li><a className="no-underline text-[#444444] font-bold inline-block px-5 py-2.5 transition-all duration-200
+                border-b-transparent hover:border-b-10 hover:border-[#2000fc]" href="/contato"> Contato </a></li>
+
+            <li><a className="no-underline text-[#444444] font-bold inline-block px-5 py-2.5 transition-all duration-200
+                border-b-transparent hover:border-b-10 hover:border-[#2000fc]" href="/participantes"> Sobre nós </a></li>
+            </ul>
+        </nav>
+        
         </>
     )
 }
--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,6 +1,9 @@
 export default function Rodape() {
     return(
         <>
+        <footer className="flex justify-center items-center h-20 bg-[url('/background-page.png')] bg-cover text-white text-lg">
+            <p>© 2025 Tech de Bairro. Todos os direitos reservados.</p>
+        </footer>
         </>
     )
 }
